24b072926aa6098bd4f5bdec03c24c85bdb400b6
zaxel <sae11.z.alex@gmail.com>
2026-04-14 22:43:10 +0300

    feat: set up fonts and basic layout.

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='h-20 bg-gray-200'>
+            footer
+        </div>
+    );
+};
+
+export default Footer;
---
+++ components/layout/Header.tsx
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='h-20 bg-gray-200'>
+            header
+        </div>
+    );
+};
+
+export default Header;
